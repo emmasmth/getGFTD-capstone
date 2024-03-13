7e9480280721008e05053f5d87790db7b4b4eb8c
emmasmth <90426359+emmasmth@users.noreply.github.com>
2024-03-12 21:44:17 -0400

    Revert "adding existing code"
    This reverts commit b673d3f951cd63329c9d3f10c58c27368eb1e292.

--- src/Constants/Modals/index.js
+++
@@ -1,4 +0,0 @@
-import SortModal from "./SortModal";
-import LoadingModal from "./LoadingModal";
-
-export { SortModal };
--- src/Constants/index.js
+++
@@ -1,9 +0,0 @@
-import Card from './Card';
-import Header from './Header';
-import Button from './Button';
-
-export {
-    Card,
-    Header,
-    Button,
-}
